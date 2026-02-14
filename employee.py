59b0d82f993b278452852ec72ac5b7289bb67ae5
NaveenaVP0405 <vpnaveena4@gmail.com>
2026-02-14 20:58:13 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,10 @@
+class Employee:
+ def __init__(self, emp_id, name, salary):
+   self.emp_id = emp_id
+   self.name = name
+   self.salary = salary
+def display(self):
+  print(f"ID: {self.emp_id}, Name: {self.name}, Salary: {self.salary}")
+def hike_salary(self, percent):
+self.salary += self.salary * percent / 100
+print(f"Updated Salary for {self.name}: {self.salary}")
